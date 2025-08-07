b82329c8bfda2095f8036f99844e3312e16c05cc
Jack Conneely <jack@conneely.org>
2025-08-07 15:40:56 -0500

    Initial backend - server & post structure

---
+++ models/Post.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    imgURL: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        required: Date.now,
+    }
+})
+
+const Post = mongoose.model('Post', postSchema);
+export default Post;
---
+++ server.js
@@ -0,0 +1,64 @@
+import express from 'express';
+import Post from './models/Post.js';
+import mongoose from 'mongoose';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/terra-blog').then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Could not connect to MongoDB:', err);
+})
+
+// Get all posts
+app.get('/api/posts', async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Get a specific post
+app.get('/api/posts/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post) {
+            return res.status(404).json({ message: 'Post not found'});
+        }
+        res.json(post);
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+});
+
+// Create a new post
+app.post('/api/posts', async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+  });
+
+  try {
+    const newPost = await post.save();
+    res.status(201).json(newPost);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Blog Backend!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
